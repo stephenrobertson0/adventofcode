3d893e104e1da96bde20298fa311cc3cffc9c344
Stephen Robertson <stephen@impact.com>
2022-12-13 08:59:21 +0200

    2022 Day 13 a

---
+++ src/main/java/_2022/_13.java
@@ -0,0 +1,165 @@
+package _2022;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class _13 {
+    
+    private static class Node {
+        Node parent;
+        List<Node> nodes = new ArrayList<>();
+        Integer value;
+    
+        @Override
+        public String toString() {
+            return "Node{" +
+                    "nodes=" + nodes +
+                    ", value=" + value +
+                    '}';
+        }
+    }
+    
+    private static int compareTo(Node left, Node right) {
+        
+        if (left.value != null && right.value != null) {
+            //System.out.println("comparing: " + left.value + " and " + right.value);
+            
+            return right.value - left.value;
+        }
+        
+        if (left.value == null && right.value == null) {
+            
+            int index = 0;
+            
+            while (true) {
+                if (index >= left.nodes.size()) {
+                    // Scenario where both lists ran out of items
+                    if (index >= right.nodes.size()) {
+                        return 0;
+                    }
+                    
+                    return 1;
+                }
+    
+                if (index >= right.nodes.size()) {
+                    // Scenario where both lists ran out of items
+                    if (index >= left.nodes.size()) {
+                        return 0;
+                    }
+                    
+                    return -1;
+                }
+        
+                int compare = compareTo(left.nodes.get(index), right.nodes.get(index));
+                if (compare != 0) {
+                    return compare;
+                }
+                
+                index++;
+            }
+        }
+        
+        if (left.value != null) {
+            Node newLeft = new Node();
+            newLeft.value = left.value;
+            left.value = null;
+            
+            left.nodes.add(newLeft);
+            
+            return compareTo(left, right);
+        }
+    
+        if (right.value != null) {
+            Node newRight = new Node();
+            newRight.value = right.value;
+            right.value = null;
+            
+            right.nodes.add(newRight);
+        
+            return compareTo(left, right);
+        }
+    
+        throw new RuntimeException();
+    }
+    
+    public static void a() throws Exception {
+    
+        BufferedReader fileReader = new BufferedReader(new FileReader("./src/main/java/_2022/input/input13.txt"));
+        
+        int index = 1;
+        long total = 0;
+        
+        while (true) {
+    
+            final String lineLeft = fileReader.readLine();
+            final String lineRight = fileReader.readLine();
+    
+            Node left = parseNodes(lineLeft);
+            Node right = parseNodes(lineRight);
+    
+            //System.out.println("COMPARE: " + compareTo(left, right));
+            
+            if (compareTo(left, right) > 0) {
+                total += index;
+            }
+    
+            index ++;
+            final String space = fileReader.readLine();
+    
+            if (space == null) {
+                break;
+            }
+        }
+    
+        System.out.println(total);
+        
+    }
+    
+    private static Node parseNodes(String line) {
+    
+        Node current = new Node();
+        
+        for (int j = 0; j < line.length(); j ++) {
+            char c = line.charAt(j);
+            
+            if (c == '[') {
+                Node node = new Node();
+                node.parent = current;
+                current.nodes.add(node);
+                current = node;
+            } else if (c == ',') {
+                continue;
+            } else if (c == ']') {
+                current = current.parent;
+            } else {
+                Node node = new Node();
+                
+                String s = ""+c;
+                
+                // Cater for 2 digit number
+                if (j+1 < line.length() && line.charAt(j+1) != ',' && line.charAt(j+1) != ']') {
+                    j++;
+                    s += line.charAt(j);
+                }
+                
+                node.value = Integer.parseInt(s);
+                current.nodes.add(node);
+            }
+        }
+        
+        return current;
+    }
+    
+    public static void b() throws Exception {
+    
+        
+    }
+    
+    public static void main(String[] args) throws Exception {
+        a();
+        b();
+    }
+}

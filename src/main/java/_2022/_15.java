1727cff4b4b11075ce1f6601495696ddcfb2612f
Stephen Robertson <stephen@impact.com>
2022-12-15 12:28:36 +0200

    2022 Day 15 b

--- src/main/java/_2022/_15.java
+++ src/main/java/_2022/_15.java
@@ -2,8 +2,7 @@ package _2022;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 
 public class _15 {
@@ -24,6 +23,77 @@ public class _15 {
                     "x=" + x +
                     ", y=" + y +
                     '}';
+        }
+    
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            Coord coord = (Coord)o;
+            return x == coord.x && y == coord.y;
+        }
+    
+        @Override
+        public int hashCode() {
+            return Objects.hash(x, y);
+        }
+    }
+    
+    private static class Range {
+    
+        public Range(int start, int end) {
+            this.start = start;
+            this.end = end;
+        }
+    
+        int start;
+        int end;
+    
+        @Override
+        public String toString() {
+            return "Range{" +
+                    "start=" + start +
+                    ", end=" + end +
+                    '}';
+        }
+    }
+    
+    private static class Row {
+        List<Range> ranges = new ArrayList<>();
+        
+        private void removeRange(Range otherRange) {
+    
+            //System.out.println("Ranges before: " + ranges);
+            //System.out.println("Removing: " + otherRange);
+            
+            List<Range> newRanges = new ArrayList<>();
+            
+            for (Range range : ranges) {
+                if (otherRange.start <= range.start && otherRange.end >= range.end) {
+                    // Don't add to newRanges
+                } else if (otherRange.start > range.start && otherRange.end < range.end) {
+                    newRanges.add(new Range(range.start, otherRange.start-1));
+                    newRanges.add(new Range(otherRange.end+1, range.end));
+                } else if (otherRange.start > range.start && otherRange.start <= range.end) {
+                    range.end = otherRange.start - 1;
+                    newRanges.add(range);
+                } else if (otherRange.end < range.end && otherRange.end >= range.start) {
+                    range.start = otherRange.end + 1;
+                    newRanges.add(range);
+                } else {
+                    // Not overlapping at all
+                    newRanges.add(range);
+                }
+            }
+            
+            ranges = newRanges;
+    
+            //System.out.println("Ranges after: " + ranges);
+            
         }
     }
     
@@ -101,7 +171,83 @@ public class _15 {
     
     public static void b() throws Exception {
     
+        BufferedReader fileReader = new BufferedReader(new FileReader("./src/main/java/_2022/input/input15.txt"));
     
+        List<Coord> allSensors = new ArrayList<>();
+        List<Coord> allBeacons = new ArrayList<>();
+        
+        int maxSize = 4000000;
+        
+        while (true) {
+    
+            final String line = fileReader.readLine();
+    
+            if (line == null) {
+                break;
+            }
+    
+            String firstPart = line.substring(0, line.indexOf(":"));
+            String secondPart = line.substring(line.indexOf(":"));
+    
+            Coord sensor = new Coord(
+                    Integer.parseInt(firstPart.substring(firstPart.indexOf("x=") + 2, firstPart.indexOf(","))),
+                    Integer.parseInt(firstPart.substring(firstPart.indexOf("y=") + 2)));
+    
+            Coord beacon = new Coord(
+                    Integer.parseInt(secondPart.substring(secondPart.indexOf("x=") + 2, secondPart.indexOf(","))),
+                    Integer.parseInt(secondPart.substring(secondPart.indexOf("y=") + 2)));
+    
+            allSensors.add(sensor);
+            allBeacons.add(beacon);
+        }
+        
+        List<Row> rows = new ArrayList<>();
+        
+        for (int k = 0; k <= maxSize; k++) {
+            Row row = new Row();
+            row.ranges.add(new Range(0, maxSize));
+            rows.add(row);
+        }
+        
+        for (int j = 0; j < allBeacons.size(); j++) {
+            
+            Coord sensor = allSensors.get(j);
+            Coord beacon = allBeacons.get(j);
+            
+            int beaconYDistance = Math.abs(sensor.y - beacon.y);
+            int beaconXDistance = Math.abs(sensor.x - beacon.x);
+    
+            int beaconTotalDistance = beaconXDistance + beaconYDistance;
+            
+            for (int k = 0; k <= maxSize; k++) {
+                Row row = rows.get(k);
+    
+                //System.out.println("ROW: " + k);
+                
+                int checkRow = k;
+    
+                int rowYDistanceFromSensor = Math.abs(sensor.y - checkRow);
+    
+                if (rowYDistanceFromSensor <= beaconTotalDistance) {
+        
+                    int distanceDifference = Math.abs(beaconTotalDistance - rowYDistanceFromSensor);
+        
+                    int xStartCoverage = sensor.x - distanceDifference;
+                    int xEndCoverage = sensor.x + distanceDifference;
+        
+                    row.removeRange(new Range(xStartCoverage, xEndCoverage));
+        
+                }
+            }
+            
+        }
+    
+        for (int k = 0; k <= maxSize; k++) {
+            if (!rows.get(k).ranges.isEmpty()) {
+                //System.out.println(rows.get(k).ranges + " " + k);
+                System.out.println((long)rows.get(k).ranges.get(0).start * 4000000 + k);
+            }
+        }
         
     }
     

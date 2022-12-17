34413955ff1baa4c583404bbd2df6811e075732d
Stephen Robertson <stephen@impact.com>
2022-12-17 12:50:23 +0200

    2022 Day 17 b

--- src/main/java/_2022/_17.java
+++ src/main/java/_2022/_17.java
@@ -2,8 +2,7 @@ package _2022;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 
@@ -46,6 +45,10 @@ public class _17 {
             char c = dirStr.charAt(index++ % dirStr.length());
             return c == '<' ? -1 : 1;
         }
+        
+        private int getIndex() {
+            return index % dirStr.length();
+        }
     }
     
     /*private static class Block {
@@ -67,6 +70,10 @@ public class _17 {
         private List<Coord> getNextBlock() {
             return blocks.get(index++ % 5);
         }
+    
+        private int getIndex() {
+            return index % 5;
+        }
     }
     
     public static void a() throws Exception {
@@ -81,7 +88,7 @@ public class _17 {
         
         long heighestRock = -1;
         
-        for (int j = 0; j < 2000000; j++) {
+        for (int j = 0; j < 2022; j++) {
         
             List<Coord> currentBlock = blocks.getNextBlock().stream().map(v->v.clone()).collect(Collectors.toList());
             
@@ -193,6 +200,16 @@ public class _17 {
         System.out.println("---------");
     }
     
+    private static class State {
+        private long height;
+        private long iteration;
+    
+        public State(long height, long iteration) {
+            this.height = height;
+            this.iteration = iteration;
+        }
+    }
+    
     public static void b() throws Exception {
         BufferedReader fileReader = new BufferedReader(new FileReader("./src/main/java/_2022/input/input17.txt"));
     
@@ -200,16 +217,26 @@ public class _17 {
     
         Directions directions = new Directions(0, line);
         Blocks blocks = new Blocks();
-        int[][] chamber = new int[7][100_000_000];
+        int[][] chamber = new int[7][4000000];
+        long[] heights = new long[]{-1, -1, -1, -1, -1, -1, -1};
+    
+        Map<String, State> simpleContours = new HashMap<>();
     
         long heighestRock = -1;
-        long yShift = 0;
     
-        for (int j = 0; j < 1_000_000_000_000L; j++) {
+        String selectedContour;
+        long heightAdded = 0;
+        long iterationsToAddHeight = 0;
+        
+        long goalIterations = 1_000_000_000_000L;
+    
+        int j;
+        
+        for (j = 0; j < 10000; j++) {
         
             List<Coord> currentBlock = blocks.getNextBlock().stream().map(v->v.clone()).collect(Collectors.toList());
         
-            Coord startPos = new Coord(2, heighestRock - yShift + 4);
+            Coord startPos = new Coord(2, heighestRock + 4);
         
             for (Coord coord : currentBlock) {
                 coord.x += startPos.x;
@@ -263,36 +290,137 @@ public class _17 {
                 chamber[(int)coord.x][(int)coord.y] = 1;
             }
         
+            //printChamber(chamber, 200, 0);
+        
             for (Coord coord : currentBlock) {
-                if (coord.y > heighestRock - yShift) {
-                    heighestRock = coord.y + yShift;
+                if (coord.y > heighestRock) {
+                    heighestRock = coord.y;
                 }
-            }
-    
-            //printChamber(chamber, currentBlock);
             
-            if (heighestRock - yShift > (100_000_000-10)) {
-    
-                int[][] newChamber = new int[7][100_000_000];
-                
-                for (int m = 0; m < 100; m++) {
-                    for (int n = 0; n < 7; n++) {
-                        newChamber[n][m] = chamber[n][(int)(heighestRock - yShift - 99 + m)];
+                for (int k = 0; k < 7; k ++) {
+                    if (coord.x == k && coord.y > heights[k]) {
+                        heights[k] = coord.y;
                     }
                 }
-    
-                //System.out.println("Chamber");
-                //printChamber(chamber, 1000-1, 1000-1 - 100);
-                //System.out.println("New Chamber");
-                //printChamber(newChamber, 100, 0);
-                
-                chamber = newChamber;
-                yShift += (heighestRock - yShift) - 99;
             }
+        
+            long min = Arrays.stream(heights).min().getAsLong();
+            int[] contour = new int[7];
+        
+            for (int k = 0; k < 7; k ++) {
+                contour[k] = (int)(heights[k] - min);
+            }
+        
+            boolean simpleContour = true;
+            for (int k = 1; k < 7; k ++) {
+                if (Math.abs(contour[k-1] - contour[k]) > 1) {
+                    simpleContour = false;
+                }
+            }
+        
+            if (simpleContour) {
+                //System.out.println("Simple contour: ");
             
+                String contourStr = Arrays.stream(contour).mapToObj(v->""+v).collect(Collectors.joining(","));
+            
+                contourStr += " - " + directions.getIndex() + " - " + blocks.getIndex();
+            
+                //System.out.println(contourStr);
+            
+                if (simpleContours.containsKey(contourStr)) {
+                    //System.out.println("Already exists!!");
+                    //System.out.println("Height added: " + (heighestRock - simpleContours.get(contourStr).height));
+                    //System.out.println("Iterations since last: " + (j - simpleContours.get(contourStr).iteration));
+                    heightAdded = heighestRock - simpleContours.get(contourStr).height;
+                    iterationsToAddHeight = j - simpleContours.get(contourStr).iteration;
+                    break;
+                }
+            
+                //System.out.println();
+            
+                simpleContours.put(contourStr, new State(heighestRock, j));
+            }
+        }
+        
+        long iterationsToGo = goalIterations - (j + 1);
+    
+        for (j = 0; j < iterationsToGo % iterationsToAddHeight; j++) {
+        
+            List<Coord> currentBlock = blocks.getNextBlock().stream().map(v->v.clone()).collect(Collectors.toList());
+        
+            Coord startPos = new Coord(2, heighestRock + 4);
+        
+            for (Coord coord : currentBlock) {
+                coord.x += startPos.x;
+                coord.y += startPos.y;
+            }
+        
+            while (true) {
+            
+                int currentDir = directions.getNextDir();
+            
+                boolean canMoveHorizontally = true;
+            
+                for (Coord coord : currentBlock) {
+                    if (coord.x + currentDir >= 7 || coord.x + currentDir < 0 || chamber[(int)coord.x + currentDir][(int)coord.y] == 1) {
+                        canMoveHorizontally = false;
+                        break;
+                    }
+                }
+            
+                if (canMoveHorizontally) {
+                    for (Coord coord : currentBlock) {
+                        coord.x += currentDir;
+                    }
+                }
+            
+                //System.out.println("Moved horizontally: " + currentDir);
+                //printChamber(chamber, currentBlock);
+            
+                boolean canMoveDown = true;
+            
+                for (Coord coord : currentBlock) {
+                    if (coord.y - 1 < 0 || chamber[(int)coord.x][(int)coord.y - 1] == 1) {
+                        canMoveDown = false;
+                        break;
+                    }
+                }
+            
+                if (!canMoveDown) {
+                    break;
+                }
+            
+                for (Coord coord : currentBlock) {
+                    coord.y -= 1;
+                }
+            
+                //System.out.println("Moved down: ");
+                //printChamber(chamber, currentBlock);
+            }
+        
+            for (Coord coord : currentBlock) {
+                chamber[(int)coord.x][(int)coord.y] = 1;
+            }
+        
+            //printChamber(chamber, 200, 0);
+        
+            for (Coord coord : currentBlock) {
+                if (coord.y > heighestRock) {
+                    heighestRock = coord.y;
+                }
+            
+                for (int k = 0; k < 7; k ++) {
+                    if (coord.x == k && coord.y > heights[k]) {
+                        heights[k] = coord.y;
+                    }
+                }
+            }
         }
     
+        heighestRock += (iterationsToGo / iterationsToAddHeight * heightAdded);
+        
         System.out.println(heighestRock+1);
+        
         
     }
     

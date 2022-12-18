d8e08509021a2e7ad9a164758541a3db98bc59e3
Stephen Robertson <stephen@impact.com>
2022-12-18 09:36:31 +0200

    2022 Day 18 b

--- src/main/java/_2022/_18.java
+++ src/main/java/_2022/_18.java
@@ -16,6 +16,8 @@ public class _18 {
         int y;
         int z;
         
+        int type;
+        
         Set<Node> neighbours = new HashSet<>();
         
         public Node(int x, int y, int z) {
@@ -80,34 +82,14 @@ public class _18 {
         System.out.println(total);
     }
     
-    private static int[][] board = new int[6][6];
-    private static Node2[][] nodeBoard = new Node2[6][6];
+    private static Node[][][] nodeBoard = new Node[25][25][25];
     
-    private static class Node2 {
-        
-        int x;
-        int y;
-        
-        // 1 - Rock
-        // 2 - Air
-        int type;
-        
-        Set<Node2> neighbours = new HashSet<>();
-        
-        public Node2(int x, int y, int type) {
-            this.x = x;
-            this.y = y;
-            this.type = type;
-        }
-    }
+    static int[][] dirs = new int[][]{{1,0,0}, {-1,0,0}, {0,1,0}, {0,-1,0}, {0,0,1}, {0,0,-1}};
     
-    static int[][] dirs = new int[][]{{0,1}, {0,-1}, {1,0}, {-1,0}};
-    
-    private static void setNodeAndNeighbourToExposed(Node2 node) {
+    private static void setNodeAndNeighbourToExposed(Node node) {
         node.type = 2;
-        board[node.x][node.y] = 2;
         
-        for (Node2 neighbour : node.neighbours) {
+        for (Node neighbour : node.neighbours) {
             if (neighbour.type == 0) {
                 setNodeAndNeighbourToExposed(neighbour);
             }
@@ -115,60 +97,86 @@ public class _18 {
     }
     
     public static void b() throws Exception {
+    
+        BufferedReader fileReader = new BufferedReader(new FileReader("./src/main/java/_2022/input/input18.txt"));
+    
+        while (true) {
+            final String line = fileReader.readLine();
+        
+            if (line == null) {
+                break;
+            }
+        
+            String[] split = line.split(",");
+    
+            // Add one to each coordinate because there are some zero indexes and we want the zero
+            // index to represent air.
+            Node node = new Node(Integer.parseInt(split[0])+1, Integer.parseInt(split[1])+1, Integer.parseInt(split[2])+1);
+            node.type = 1;
+            nodeBoard[node.x][node.y][node.z] = node;
+        }
+        
         // 0 == Air Unknown
         // 1 == Rock
         // 2 == Air exposed to outside
         // 3 == Air not exposed to outside
         
-        board[2][0] = 1;
-        board[3][0] = 1;
-        board[1][1] = 1;
-        board[4][1] = 1;
-        board[2][2] = 1;
-        board[4][2] = 1;
-        board[3][3] = 1;
-        board[4][3] = 1;
-        
-        for (int j = 0; j < 6; j++) {
-            for (int k = 0; k < 6; k++) {
-                System.out.print(board[k][j]);
-            }
-            System.out.println();
-        }
-        
-        for (int j = 0; j < 6; j++) {
-            for (int k = 0; k < 6; k++) {
-                nodeBoard[k][j] = new Node2(k, j, board[k][j]);
-            }
-        }
-    
-        for (int j = 0; j < 6; j++) {
-            for (int k = 0; k < 6; k++) {
-                for (int m = 0; m < 4; m++) {
-                    int x = k+dirs[m][0];
-                    int y = j+dirs[m][1];
-                    
-                    if (x >=0 && x < 6 && y >=0 && y < 6) {
-                        nodeBoard[k][j].neighbours.add(nodeBoard[x][y]);
+        for (int j = 0; j < 25; j++) {
+            for (int k = 0; k < 25; k++) {
+                for (int m = 0; m < 25; m++) {
+                    if (nodeBoard[m][k][j] == null) {
+                        nodeBoard[m][k][j] = new Node(m, k, j);
                     }
                 }
             }
         }
     
-        setNodeAndNeighbourToExposed(nodeBoard[0][0]);
-    
-        for (int j = 0; j < 6; j++) {
-            for (int k = 0; k < 6; k++) {
-                System.out.print(board[k][j]);
+        for (int j = 0; j < 25; j++) {
+            for (int k = 0; k < 25; k++) {
+                for (int m = 0; m < 25; m++) {
+                    for (int n = 0; n < 6; n++) {
+                        int x = m + dirs[n][0];
+                        int y = k + dirs[n][1];
+                        int z = j + dirs[n][2];
+        
+                        if (x >= 0 && x < 25 && y >= 0 && y < 25 && z >= 0 && z < 25) {
+                            nodeBoard[m][k][j].neighbours.add(nodeBoard[x][y][z]);
+                        }
+                    }
+                }
             }
-            System.out.println();
         }
+    
+        setNodeAndNeighbourToExposed(nodeBoard[0][0][0]);
         
+        /*for (int j = 0; j < 25; j++) {
+            for (int k = 0; k < 25; k++) {
+                for (int m = 0; m < 25; m++) {
+                    if (nodeBoard[m][k][j].type == 0) {
+                        nodeBoard[m][k][j].type = 3;
+                        System.out.println("Bubble at: " + m + " " + k + " " + j);
+                    }
+                }
+            }
+        }*/
+    
+        int total = 0;
         
+        for (int j = 0; j < 25; j++) {
+            for (int k = 0; k < 25; k++) {
+                for (int m = 0; m < 25; m++) {
+                    if (nodeBoard[m][k][j].type == 1) {
+                        total += nodeBoard[m][k][j].neighbours.stream().filter(v -> v.type == 2).count();
+                    }
+                }
+            }
+        }
+    
+        System.out.println(total);
     }
     
     public static void main(String[] args) throws Exception {
-        //a();
+        a();
         b();
     }
 }

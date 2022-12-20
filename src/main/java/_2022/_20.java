5380544acf7348f234182e74071712f49c9333eb
Stephen Robertson <stephen@impact.com>
2022-12-20 10:49:46 +0200

    2022 Day 20 - b

--- src/main/java/_2022/_20.java
+++ src/main/java/_2022/_20.java
@@ -2,19 +2,22 @@ package _2022;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
 public class _20 {
     
     private static class Number {
-        int value;
-    
-        public Number(int value) {
+        
+        long value;
+        
+        public Number(long value) {
             this.value = value;
         }
-    
+        
         @Override
         public String toString() {
             return "Number{" +
@@ -23,27 +26,38 @@ public class _20 {
         }
     }
     
-    private static Map<Number, Integer> indexMap = new HashMap<>();
-
     
+    private static Map<Number, Integer> indexMap = new HashMap<>();
+    
+    private static int getNewIndex(int index, long moveAmount, int listSize) {
+        int newIndex = (int)((index + moveAmount + (listSize - 1) * 10000000000000L) % (listSize - 1));
+        
+        return newIndex;
+    }
+    
+    private static void updateList(List<Number> list, int oldIndex, int newIndex) {
+        Number number = list.remove(oldIndex);
+    
+        list.add(newIndex, number);
+    }
     
     public static void a() throws Exception {
-    
+        
         BufferedReader fileReader = new BufferedReader(new FileReader("./src/main/java/_2022/input/input20.txt"));
-    
+        
         List<Number> originalList = new ArrayList<>();
         List<Number> updatedList = new ArrayList<>();
-    
+        
         int index = 0;
         
         while (true) {
-    
+            
             final String line = fileReader.readLine();
-    
+            
             if (line == null) {
                 break;
             }
-    
+            
             Number number = new Number(Integer.parseInt(line));
             originalList.add(number);
             updatedList.add(number);
@@ -51,30 +65,93 @@ public class _20 {
         }
         
         for (Number number : originalList) {
-    
-            Integer i = indexMap.remove(number);
-            updatedList.remove((int)i);
             
-            int newIndex = ((i + number.value + 1 + (originalList.size() - 1) * 10000)) % (originalList.size() - 1);
+            Integer i = indexMap.get(number);
             
-            if (newIndex > i) {
-                newIndex -= 1;
-            }
+            int newIndex = getNewIndex(i, number.value, updatedList.size());
             
-            updatedList.add(newIndex, number);
-            indexMap.put(number, newIndex);
+            updateList(updatedList, i, newIndex);
             
-            for (int j = newIndex; j < updatedList.size(); j++) {
+            for (int j = 0; j < updatedList.size(); j++) {
                 indexMap.put(updatedList.get(j), j);
             }
-    
-            System.out.println(updatedList);
+            
+            //System.out.println(updatedList);
         }
         
+        int zeroIndex = -1;
+        
+        for (Number number : updatedList) {
+            if (number.value == 0) {
+                zeroIndex = indexMap.get(number);
+            }
+        }
+        
+        //System.out.println(zeroIndex);
+        
+        System.out.println(updatedList.get((zeroIndex + 1000) % updatedList.size()).value + updatedList.get(
+                (zeroIndex + 2000) % updatedList.size()).value + updatedList.get(
+                (zeroIndex + 3000) % updatedList.size()).value);
+    }
+    
+    public static void b() throws Exception {
+        
+        BufferedReader fileReader = new BufferedReader(new FileReader("./src/main/java/_2022/input/input20.txt"));
+        
+        List<Number> originalList = new ArrayList<>();
+        List<Number> updatedList = new ArrayList<>();
+        
+        int index = 0;
+        
+        while (true) {
+            
+            final String line = fileReader.readLine();
+            
+            if (line == null) {
+                break;
+            }
+            
+            Number number = new Number(Integer.parseInt(line) * 811589153L);
+            originalList.add(number);
+            updatedList.add(number);
+            indexMap.put(number, index);
+        }
+        
+        for (int k = 0; k < 10; k++) {
+            for (Number number : originalList) {
+        
+                Integer i = indexMap.get(number);
+        
+                int newIndex = getNewIndex(i, number.value, updatedList.size());
+        
+                updateList(updatedList, i, newIndex);
+        
+                for (int j = 0; j < updatedList.size(); j++) {
+                    indexMap.put(updatedList.get(j), j);
+                }
+        
+                //System.out.println(updatedList);
+            }
+        }
+        
+        int zeroIndex = -1;
+        
+        for (Number number : updatedList) {
+            if (number.value == 0) {
+                zeroIndex = indexMap.get(number);
+            }
+        }
+        
+        //System.out.println(zeroIndex);
+        
+        System.out.println(updatedList.get((zeroIndex + 1000) % updatedList.size()).value + updatedList.get(
+                (zeroIndex + 2000) % updatedList.size()).value + updatedList.get(
+                (zeroIndex + 3000) % updatedList.size()).value);
     }
     
     public static void main(String[] args) throws Exception {
         a();
-        //b();
+        b();
+        
     }
 }

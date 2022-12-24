0348dfdfc394a29dc0f5922e93f94a78bd147f9d
Stephen Robertson <stephen@impact.com>
2022-12-24 10:32:54 +0200

    2022 Day 24 - b

--- src/main/java/_2022/_24.java
+++ src/main/java/_2022/_24.java
@@ -58,24 +58,47 @@ public class _24 {
     }
     
     
+    private static class BlizzardBoard {
+        
+        int[][] board = new int[XSIZE][YSIZE];
+        
+        public BlizzardBoard(List<Blizzard> blizzards) {
+            for (Blizzard blizzard : blizzards) {
+                board[blizzard.x][blizzard.y] = 1;
+            }
+        }
+    }
+    
+    
     private static class State {
         
         private int x;
         private int y;
         private int timePassed;
+        private Destination destination;
+        private int destinationIndex;
         
-        public State(int x, int y, int timePassed) {
+        public State(
+                int x,
+                int y,
+                int timePassed,
+                Destination destination,
+                int destinationIndex) {
             this.x = x;
             this.y = y;
             this.timePassed = timePassed;
+            this.destination = destination;
+            this.destinationIndex = destinationIndex;
         }
-        
+    
         @Override
         public String toString() {
             return "State{" +
                     "x=" + x +
                     ", y=" + y +
                     ", timePassed=" + timePassed +
+                    ", destination=" + destination +
+                    ", destinationIndex=" + destinationIndex +
                     '}';
         }
     
@@ -88,12 +111,14 @@ public class _24 {
                 return false;
             }
             State state = (State)o;
-            return x == state.x && y == state.y && timePassed == state.timePassed;
+            return x == state.x && y == state.y && timePassed == state.timePassed
+                    && destinationIndex == state.destinationIndex
+                    && Objects.equals(destination, state.destination);
         }
     
         @Override
         public int hashCode() {
-            return Objects.hash(x, y, timePassed);
+            return Objects.hash(x, y, timePassed, destination, destinationIndex);
         }
     }
     
@@ -102,7 +127,7 @@ public class _24 {
         int[][] board = new int[XSIZE][YSIZE];
         
         for (Blizzard blizzard : blizzards) {
-            board[blizzard.x][blizzard.y] = blizzard.direction+4;
+            board[blizzard.x][blizzard.y] = blizzard.direction + 4;
         }
         
         if (state.y >= 0) {
@@ -120,7 +145,18 @@ public class _24 {
         
     }
     
-    public static void a() throws Exception {
+    private static class Destination {
+        
+        int destinationX;
+        int destinationY;
+        
+        public Destination(int destinationX, int destinationY) {
+            this.destinationX = destinationX;
+            this.destinationY = destinationY;
+        }
+    }
+    
+    public static int getStepCount(List<Destination> destinations) throws Exception {
         
         BufferedReader fileReader = new BufferedReader(new FileReader("./src/main/java/_2022/input/input24.txt"));
         
@@ -128,6 +164,7 @@ public class _24 {
         
         List<Blizzard> blizzards = new ArrayList<>();
         Map<Integer, List<Blizzard>> blizzardsByTime = new HashMap<>();
+        Map<Integer, BlizzardBoard> blizzardsBoardsByTime = new HashMap<>();
         
         int y = 0;
         
@@ -174,92 +211,89 @@ public class _24 {
             y++;
         }
         
-        for (int j = 1; j < 10000; j++) {
+        for (int j = 1; j < 1000; j++) {
             
             List<Blizzard> newBlizzards = new ArrayList<>();
             
-            for (Blizzard blizzard : blizzardsByTime.get(j-1)) {
+            for (Blizzard blizzard : blizzardsByTime.get(j - 1)) {
                 newBlizzards.add(blizzard.move());
             }
             
             blizzardsByTime.put(j, newBlizzards);
+            blizzardsBoardsByTime.put(j, new BlizzardBoard(newBlizzards));
             
         }
         
-        State startState = new State(0, -1, 0);
+        State startState = new State(
+                0,
+                -1,
+                0,
+                destinations.get(0),
+                0);
         
         List<State> allStates = new ArrayList<>();
         allStates.add(startState);
         
         while (true) {
-    
+            
             List<State> newAllStates = new ArrayList<>();
             
             for (State state : allStates) {
-        
+                
                 //printState(state, blizzardsByTime.get(state.timePassed));
-        
+                
                 for (int j = 0; j < 5; j++) {
-            
+                    
                     int newX = state.x + myMoves[j][0];
                     int newY = state.y + myMoves[j][1];
                     int newTimePassed = state.timePassed + 1;
-            
-                    if (newX < 0 || newX == XSIZE || newY < 0 || newY == YSIZE) {
+                    
+                    if (newX < 0 || newX >= XSIZE || newY < 0 || newY >= YSIZE) {
                         continue;
                     }
-            
-                    List<Blizzard> currentBlizzards = blizzardsByTime.get(newTimePassed);
-            
+                    
+                    BlizzardBoard blizzardBoard = blizzardsBoardsByTime.get(newTimePassed);
+                    
                     boolean blizzardBlocking = false;
-            
-                    for (Blizzard currentBlizzard : currentBlizzards) {
-                        if (currentBlizzard.x == newX && currentBlizzard.y == newY) {
-                            blizzardBlocking = true;
-                            break;
-                        }
+                    
+                    if (blizzardBoard.board[newX][newY] == 1) {
+                        blizzardBlocking = true;
                     }
-            
+                    
                     if (!blizzardBlocking) {
-                        State newState = new State(newX, newY, newTimePassed);
+                        State newState = new State(
+                                newX,
+                                newY,
+                                newTimePassed,
+                                state.destination,
+                                state.destinationIndex);
                         if (!newAllStates.contains(newState)) {
                             newAllStates.add(newState);
                         }
                     }
-            
+                    
                 }
             }
-    
-            //System.out.println(newAllStates);
             
-            /*int maxDistance = 0;
-    
-            for (State s : newAllStates) {
-                int distance = s.x + s.y;
-        
-                if (distance > maxDistance) {
-                    maxDistance = distance;
-                }
-            }
-    
-            allStates = new ArrayList<>();
-    
-            for (State s : newAllStates) {
-                int distance = s.x + s.y;
-        
-                if (distance > maxDistance - 30) {
-                    allStates.add(s);
-                }
-            }*/
-            
-            allStates = newAllStates;
-    
             boolean finished = false;
             
-            for (State s : allStates) {
-                if (s.x == XSIZE - 1 && s.y == YSIZE - 1) {
-                    finished = true;
+            allStates = new ArrayList<>();
+            
+            for (State s : newAllStates) {
+                if (s.x == s.destination.destinationX && s.y == s.destination.destinationY) {
+                    s.y += s.destination.destinationY == 0 ? -1 : 1;
+                    s.timePassed += 1;
+                    s.destinationIndex += 1;
+                    
+                    if (s.destinationIndex != destinations.size()) {
+                        s.destination = destinations.get(s.destinationIndex);
+                    } else {
+                        s.destination = null;
+                        finished = true;
+                    }
                 }
+                
+                allStates.add(s);
             }
             
             if (finished) {
@@ -267,15 +301,24 @@ public class _24 {
             }
             
         }
+        
+        return allStates.stream().mapToInt(v->v.timePassed).max().getAsInt();
+        
+    }
     
-        System.out.println(allStates.get(0).timePassed+1);
+    public static void a() throws Exception {
+        
+        System.out.println(getStepCount(Arrays.asList(new Destination(XSIZE - 1, YSIZE - 1))));
         
     }
     
     public static void b() throws Exception {
-    
-    
-    
+        
+        System.out.println(getStepCount(Arrays.asList(
+                new Destination(XSIZE - 1, YSIZE - 1),
+                new Destination(0, 0),
+                new Destination(XSIZE - 1, YSIZE - 1))));
+        
     }
     
     public static void main(String[] args) throws Exception {

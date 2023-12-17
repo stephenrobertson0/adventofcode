550a7f3549804e3988f0386b5c40382a07d5152b
Stephen Robertson <stephen@impact.com>
2023-12-17 09:30:29 +0200

    2023 Day 17

--- src/main/java/_2023/_17.java
+++ src/main/java/_2023/_17.java
@@ -2,14 +2,10 @@ package _2023;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.util.ArrayList;
+import java.util.ArrayDeque;
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
 import java.util.Map;
-import java.util.Objects;
-import java.util.Set;
-import java.util.Stack;
+import java.util.Queue;
 
 
 public class _17 {
@@ -67,7 +63,7 @@ public class _17 {
         }
     }
 
-    public static void a() throws Exception {
+    private static int getMinHeatLoss(int minStraight, int maxStraight) throws Exception {
         BufferedReader fileReader = new BufferedReader(new FileReader("./src/main/java/_2023/input/input17.txt"));
 
         int yIndex = 0;
@@ -87,17 +83,17 @@ public class _17 {
 
         }
 
-        for (int j = 0; j < size; j++) {
+        /*for (int j = 0; j < size; j++) {
             for (int k = 0; k < size; k++) {
                 System.out.print(land[k][j] + " ");
             }
             System.out.println();
-        }
+        }*/
 
-        Stack<Position> positions = new Stack<>();
+        Queue<Position> positions = new ArrayDeque<>();
 
-        positions.push(new Position(0, 0, 0, 1, 0));
-        positions.push(new Position(0, 0, 0, 3, 0));
+        positions.add(new Position(0, 0, 0, 1, 0));
+        positions.add(new Position(0, 0, 0, 2, 0));
 
         Map<String, Integer> visited = new HashMap<>();
 
@@ -105,9 +101,7 @@ public class _17 {
 
         while (!positions.isEmpty()) {
 
-            Position position = positions.pop();
-
-            //System.out.println("Checking position: " + position);
+            Position position = positions.remove();
 
             if (position.getX() == size - 1 && position.getY() == size - 1) {
                 if (position.getHeatCount() < min) {
@@ -116,46 +110,48 @@ public class _17 {
                 continue;
             }
 
-            //visited.add(position.getX() + "," + position.getY() + "," + position.getForwardCount());
-
             int newDir1 = (position.getDirection() + 1) % 4;
             int newDir2 = (position.getDirection() + 3) % 4;
 
             int newX1 = position.getX() + increments[newDir1][0];
             int newY1 = position.getY() + increments[newDir1][1];
 
-            if (!(newX1 < 0 || newX1 >= size || newY1 < 0 || newY1 >= size)) {
+            if (!(newX1 < 0 || newX1 >= size || newY1 < 0 || newY1 >= size) && position.getForwardCount() >= minStraight) {
 
                 int newHeatCount = position.getHeatCount() + land[newX1][newY1];
 
                 int existingHeatCount = Integer.MAX_VALUE;
 
-                if (visited.containsKey(newX1 + "," + newY1 + ",0")) {
-                    existingHeatCount = visited.get(newX1 + "," + newY1 + ",0");
+                String key = newX1 + "," + newY1 + ",1" + "," + newDir1;
+
+                if (visited.containsKey(key)) {
+                    existingHeatCount = visited.get(key);
                 }
 
                 if (newHeatCount < existingHeatCount) {
-                    positions.push(new Position(newX1, newY1, 0, newDir1, newHeatCount));
-                    visited.put(newX1 + "," + newY1 + ",0", newHeatCount);
+                    positions.add(new Position(newX1, newY1, 1, newDir1, newHeatCount));
+                    visited.put(key, newHeatCount);
                 }
             }
 
             int newX2 = position.getX() + increments[newDir2][0];
             int newY2 = position.getY() + increments[newDir2][1];
 
-            if (!(newX2 < 0 || newX2 >= size || newY2 < 0 || newY2 >= size)) {
+            if (!(newX2 < 0 || newX2 >= size || newY2 < 0 || newY2 >= size) && position.getForwardCount() >= minStraight) {
 
                 int newHeatCount = position.getHeatCount() + land[newX2][newY2];
 
                 int existingHeatCount = Integer.MAX_VALUE;
 
-                if (visited.containsKey(newX2 + "," + newY2 + ",0")) {
-                    existingHeatCount = visited.get(newX2 + "," + newY2 + ",0");
+                String key = newX2 + "," + newY2 + ",1" + "," + newDir2;
+
+                if (visited.containsKey(key)) {
+                    existingHeatCount = visited.get(key);
                 }
 
                 if (newHeatCount < existingHeatCount) {
-                    positions.push(new Position(newX2, newY2, 0, newDir2, position.getHeatCount() + land[newX2][newY2]));
-                    visited.put(newX2 + "," + newY2 + ",0", newHeatCount);
+                    positions.add(new Position(newX2, newY2, 1, newDir2, newHeatCount));
+                    visited.put(key, newHeatCount);
                 }
             }
 
@@ -163,40 +159,35 @@ public class _17 {
             int newX3 = position.getX() + increments[position.getDirection()][0];
             int newY3 = position.getY() + increments[position.getDirection()][1];
 
-            if (!(newX3 < 0 || newX3 >= size || newY3 < 0 || newY3 >= size) && position.getForwardCount() < 2) {
+            if (!(newX3 < 0 || newX3 >= size || newY3 < 0 || newY3 >= size) && position.getForwardCount() < maxStraight) {
 
                 int newHeatCount = position.getHeatCount() + land[newX3][newY3];
 
                 int existingHeatCount = Integer.MAX_VALUE;
 
-                if (visited.containsKey(newX3 + "," + newY3 + "," + (position.getForwardCount()+1))) {
-                    existingHeatCount = visited.get(newX3 + "," + newY3 + "," + (position.getForwardCount()+1));
+                String key = newX3 + "," + newY3 + "," + (position.getForwardCount()+1) + "," + position.getDirection();
+
+                if (visited.containsKey(key)) {
+                    existingHeatCount = visited.get(key);
                 }
 
                 if (newHeatCount < existingHeatCount) {
-                    positions.push(new Position(newX3, newY3, position.getForwardCount() + 1, position.getDirection(), position.getHeatCount() + land[newX3][newY3]));
-                    visited.put(newX3 + "," + newY3 + "," + (position.getForwardCount()+1), newHeatCount);
+                    positions.add(new Position(newX3, newY3, position.getForwardCount() + 1, position.getDirection(), newHeatCount));
+                    visited.put(key, newHeatCount);
                 }
             }
 
         }
 
-        System.out.println(min);
+        return min;
+    }
 
+    public static void a() throws Exception {
+        System.out.println(getMinHeatLoss(0, 3));
     }
 
     public static void b() throws Exception {
-        BufferedReader fileReader = new BufferedReader(new FileReader("./src/main/java/_2023/input/input17.txt"));
-
-        while (true) {
-            final String line = fileReader.readLine();
-
-            if (line == null) {
-                break;
-            }
-
-        }
-
+        System.out.println(getMinHeatLoss(4, 10));
     }
     
     public static void main(String[] args) throws Exception {

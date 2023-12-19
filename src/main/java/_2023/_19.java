e81856430ba64adc7895e61fe9daf8675d32d77c
Stephen Robertson <stephen@impact.com>
2023-12-19 08:58:50 +0200

    2023 Day 19 - Part 2 (Wrong answer)

--- src/main/java/_2023/_19.java
+++ src/main/java/_2023/_19.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Stack;
 
 
 public class _19 {
@@ -148,17 +149,173 @@ public class _19 {
 
     }
 
+    private static class MinAndMax {
+        private int min;
+        private int max;
+
+        public MinAndMax(int min, int max) {
+            this.min = min;
+            this.max = max;
+        }
+
+        public int getMin() {
+            return min;
+        }
+
+        public int getMax() {
+            return max;
+        }
+
+        @Override
+        public String toString() {
+            return "MinAndMax{" +
+                    "min=" + min +
+                    ", max=" + max +
+                    '}';
+        }
+    }
+
+    private static class State {
+        private String location;
+        private Map<Character, MinAndMax> minAndMaxMap;
+
+        public State(String location, Map<Character, MinAndMax> minAndMaxMap) {
+            this.location = location;
+            this.minAndMaxMap = minAndMaxMap;
+        }
+
+        public String getLocation() {
+            return location;
+        }
+
+        public Map<Character, MinAndMax> getMinAndMaxMap() {
+            return minAndMaxMap;
+        }
+
+        @Override
+        public String toString() {
+            return "State{" +
+                    "location='" + location + '\'' +
+                    ", minAndMaxMap=" + minAndMaxMap +
+                    '}';
+        }
+    }
+
     public static void b() throws Exception {
         BufferedReader fileReader = new BufferedReader(new FileReader("./src/main/java/_2023/input/input19.txt"));
+
+        Map<String, List<Condition>> map = new HashMap<>();
 
         while (true) {
             final String line = fileReader.readLine();
 
-            if (line == null) {
+            if (line.isEmpty()) {
                 break;
             }
 
+            List<Condition> conditionList = new ArrayList<>();
+
+            String name = line.substring(0, line.indexOf("{"));
+
+            String conditions = line.substring(line.indexOf("{") + 1, line.length() - 1);
+
+            String[] cond = conditions.split(",");
+
+            for (int i = 0; i < cond.length; i++) {
+                String c = cond[i];
+
+                if (c.contains(":")) {
+                    conditionList.add(new Condition(c.charAt(0), c.charAt(1), Integer.parseInt(c.substring(2, c.indexOf(":"))), c.substring(c.indexOf(":") + 1)));
+                } else {
+                    conditionList.add(new Condition((char)0, (char)0, 0, c));
+                }
+            }
+
+            map.put(name, conditionList);
         }
+
+
+        Stack<State> states = new Stack<>();
+
+        Map<Character, MinAndMax> minAndMaxMap = new HashMap<>();
+        minAndMaxMap.put('x', new MinAndMax(1, 4000));
+        minAndMaxMap.put('m', new MinAndMax(1, 4000));
+        minAndMaxMap.put('a', new MinAndMax(1, 4000));
+        minAndMaxMap.put('s', new MinAndMax(1, 4000));
+
+        states.push(new State("in", minAndMaxMap));
+
+        long total = 0;
+
+        while (!states.isEmpty()) {
+
+            State state = states.pop();
+
+            if ("R".equals(state.getLocation())) {
+                System.out.println("R hit");
+                continue;
+            }
+
+            if ("A".equals(state.getLocation())) {
+                System.out.println("A hit: " + state);
+                total += (long)(state.getMinAndMaxMap().get('x').getMax() - state.getMinAndMaxMap().get('x').getMin() + 1) * (
+                        state.getMinAndMaxMap().get('m').getMax() - state.getMinAndMaxMap().get('m').getMin() + 1) * (
+                        state.getMinAndMaxMap().get('a').getMax() - state.getMinAndMaxMap().get('a').getMin() + 1) * (
+                        state.getMinAndMaxMap().get('s').getMax() - state.getMinAndMaxMap().get('s').getMin() + 1);
+                continue;
+            }
+
+            List<Condition> condition = map.get(state.getLocation());
+
+            String nextState = "";
+
+            Map<Character, MinAndMax> passingThrough = new HashMap<>(state.getMinAndMaxMap());
+
+            for (Condition cond : condition) {
+
+                if (cond.getRating() == 0) {
+                    nextState = cond.getDestination();
+                    states.push(new State(nextState, passingThrough));
+                    break;
+                }
+
+                MinAndMax minAndMax = state.getMinAndMaxMap().get(cond.getRating());
+                if (cond.getCondition() == '<') {
+                    if (minAndMax.getMin() < cond.getValue() && minAndMax.getMax() < cond.getValue()) {
+
+                        Map<Character, MinAndMax> clone = new HashMap<>(passingThrough);
+                        states.push(new State(cond.getDestination(), clone));
+                        break;
+
+                    } else if (minAndMax.getMin() <= cond.getValue() && minAndMax.getMax() >= cond.getValue()) {
+                        Map<Character, MinAndMax> clone = new HashMap<>(passingThrough);
+
+                        clone.put(cond.getRating(), new MinAndMax(minAndMax.getMin(), cond.getValue()-1));
+                        states.push(new State(cond.getDestination(), clone));
+
+                        passingThrough.put(cond.getRating(), new MinAndMax(cond.getValue(), minAndMax.getMax()));
+                    }
+                } else {
+                    if (minAndMax.getMin() > cond.getValue() && minAndMax.getMax() > cond.getValue()) {
+
+                        Map<Character, MinAndMax> clone = new HashMap<>(passingThrough);
+                        states.push(new State(cond.getDestination(), clone));
+                        break;
+
+                    } else if (minAndMax.getMin() <= cond.getValue() && minAndMax.getMax() >= cond.getValue()) {
+                        Map<Character, MinAndMax> clone = new HashMap<>(passingThrough);
+
+                        clone.put(cond.getRating(), new MinAndMax(cond.getValue()+1, minAndMax.getMax()));
+                        states.push(new State(cond.getDestination(), clone));
+
+                        passingThrough.put(cond.getRating(), new MinAndMax(minAndMax.getMin(), cond.getValue()));
+                    }
+                }
+            }
+        }
+
+        System.out.println(total);
+        System.out.println(4000L*4000*4000*4000);
 
     }
     

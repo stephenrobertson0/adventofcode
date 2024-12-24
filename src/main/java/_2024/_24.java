d624c4447974ce1e1152282b1e47de290007ce4d
Stephen Robertson <stephen@impact.com>
2024-12-24 08:15:23 +0200

    2024 Day 24 - Part 2 - In progress

--- src/main/java/_2024/_24.java
+++ src/main/java/_2024/_24.java
@@ -112,7 +112,87 @@ public class _24 {
         System.out.println(Long.valueOf(finalStr, 2));
     }
 
+    public static void b() throws Exception {
+
+        BufferedReader fileReader = new BufferedReader(new FileReader("./src/main/java/_2024/input/input24.txt"));
+
+        Map<String, Integer> values = new HashMap<>();
+
+        while (true) {
+            final String line = fileReader.readLine();
+
+            if (line.isEmpty()) {
+                break;
+            }
+        }
+
+        String xString = new StringBuilder("101101010111101101000111000000010110000000011").reverse().toString();
+        String yString = new StringBuilder("100011100111110100010111100110001110000011011").reverse().toString();
+
+        for (int j = 0; j < xString.length(); j++) {
+            values.put("x" + ((j < 10) ? "0" : "") + j, Integer.parseInt(""+xString.charAt(j)));
+        }
+
+        for (int j = 0; j < yString.length(); j++) {
+            values.put("y" + ((j < 10) ? "0" : "") + j, Integer.parseInt(""+yString.charAt(j)));
+        }
+
+        Map<String, Func> funcs = new HashMap<>();
+        List<String> allWires = new ArrayList<>();
+
+        while (true) {
+            final String line = fileReader.readLine();
+
+            if (line == null) {
+                break;
+            }
+
+            String var1 = line.split(" ")[0];
+            String op = line.split(" ")[1];
+            String var2 = line.split(" ")[2];
+            String result = line.split(" -> ")[1];
+
+            Func func = new Func(List.of(var1, var2), op);
+
+            allWires.add(result);
+
+            funcs.put(result, func);
+        }
+
+
+        Collections.sort(allWires, Comparator.reverseOrder());
+
+        System.out.println(allWires);
+
+        Map<String, Integer> wiresAndResults = new HashMap<>();
+
+        for (String wire : allWires) {
+            wiresAndResults.put(wire, getResult(wire, values, funcs));
+        }
+
+        String finalStrZ = "";
+
+        for (String wire : allWires) {
+
+            if (wire.startsWith("z")) {
+                finalStrZ += wiresAndResults.get(wire);
+            }
+        }
+
+        System.out.println(finalStrZ);
+
+        /*String finalXString = "0" + new StringBuilder(xString).reverse();
+        String finalYString =
+
+        System.out.println();
+        System.out.println("0" + new StringBuilder(yString).reverse());*/
+
+        System.out.println(Long.valueOf(finalStrZ, 2));
+
+    }
+
     public static void main(String[] args) throws Exception {
         a();
+        b();
     }
 }
